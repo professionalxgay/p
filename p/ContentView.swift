f30e316159e00e6a956c5c8450acdb168a26b7cd
Jake Austin <austinjake15@gmail.com>
2023-11-08 18:01:02 +0000

    Initial Commit

---
+++ p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  p
+//
+//  Created by Jake Anthony Thomas Austin on 08/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ p/pApp.swift
@@ -0,0 +1,17 @@
+//
+//  pApp.swift
+//  p
+//
+//  Created by Jake Anthony Thomas Austin on 08/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
